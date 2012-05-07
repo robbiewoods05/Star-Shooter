a710bc2b8123a2796559cbf59fb1b2dca3c92cf2
Robbie Woods <robbiewoods05@gmail.com>
2012-05-07 13:41:18 +0100

    Moved variables and moved collision functions to header file "Collision.h"

--- Star Shooter/main.cpp
+++ Star Shooter/main.cpp
@@ -5,16 +5,7 @@
 #include "Spaceship.h"
 #include "Bullet.h"
 #include "Comet.h"
-
-using namespace ENUMS;
-
-// *** GLOBALS ***
-bool keys[5] = { false, false, false, false, false };
-
-
-// *** PROTOTYPES ***
-
-void collideBullet(Bullet bullet[], int bSize, Comet comets[], int cSize);
+#include "Collision.h"
 
 
 int main(void)
@@ -157,26 +148,3 @@ int main(void)
 }
 
 
-void collideBullet(Bullet bullet[], int bSize, Comet comets[], int cSize)
-{
-	for (int i = 0; i < bSize; i++)
-	{
-		if (bullet[i].live)
-		{
-			for (int j = 0; j < cSize; j++)
-			{
-				if (comets[j].live)
-				{
-					if (bullet[i].x > (comets[j].x) &&
-						bullet[i].x < (comets[j].x + comets[j].boundX) &&
-						bullet[i].y > (comets[j].y) &&
-						bullet[i].y < (comets[j].y + 18))
-					{
-						bullet[i].live = false;
-						comets[j].live = false;
-					}
-				}
-			}
-		}
-	}
-}
