333da115cb01bb82d650907200931365e8b146b4
Robbie Woods <robbiewoods05@gmail.com>
2012-05-07 13:41:45 +0100

    Added bool array

--- Star Shooter/objects.h
+++ Star Shooter/objects.h
@@ -4,3 +4,5 @@ namespace ENUMS
 	enum KEYS { UP, DOWN, LEFT, RIGHT, SPACE };
 }
 const int WIDTH = 800, HEIGHT = 400, FPS = 60, NUM_BULLETS = 5, NUM_COMETS = 10;
+
+bool keys[5] = { false, false, false, false, false };
