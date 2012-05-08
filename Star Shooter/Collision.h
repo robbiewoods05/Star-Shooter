c6b76f9970f7a82b2093db7f9985b471f9b12277
Robbie Woods <robbiewoods05@gmail.com>
2012-05-08 19:49:12 +0100

    Fixed collision detection and added detection for ship collision.

--- Star Shooter/Collision.h
+++ Star Shooter/Collision.h
@@ -11,15 +11,38 @@ void collideBullet(Bullet bullet[], int bSize, Comet comets[], int cSize)
 			{
 				if (comets[j].live)
 				{
-					if (bullet[i].x > (comets[j].x) &&
+					if (bullet[i].x > (comets[j].x - comets[j].boundX) &&
 						bullet[i].x < (comets[j].x + comets[j].boundX) &&
-						bullet[i].y > (comets[j].y) &&
-						bullet[i].y < (comets[j].y + 18))
+						bullet[i].y > (comets[j].y - comets[j].boundY) &&
+						bullet[i].y < (comets[j].y + comets[j].boundY))
 					{
 						bullet[i].live = false;
 						comets[j].live = false;
 					}
 				}
+			}
+		}
+	}
+}
+
+void collideComet(Comet comets[], int cSize, Spaceship &ship)
+{
+	for (int i = 0; i < cSize; i++)
+	{
+		if (comets[i].live)
+		{
+			if (comets[i].x - comets[i].boundX < ship.x + ship.boundX &&
+				comets[i].x + comets[i].boundX > ship.x - ship.boundX &&
+				comets[i].y - comets[i].boundY < ship.y + ship.boundY &&
+				comets[i].y + comets[i].boundY > ship.y - ship.boundY)
+			{
+				ship.lives--;
+				comets[i].live = false;
+			}
+			else if(comets[i].x < 0)
+			{
+				comets[i].live = false;
+				ship.lives--;
 			}
 		}
 	}
